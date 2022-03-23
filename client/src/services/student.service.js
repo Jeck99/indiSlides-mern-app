7347dc1450a6e38a66ba58e2cfb961d118a73dde
jeck99 <jeck99@gmail.com>
2022-03-23 17:03:33 +0200

    as

--- client/src/services/student.service.js
+++ client/src/services/student.service.js
@@ -1,5 +1,5 @@
 const BASIC_API =process.env.NODE_ENV ==='production'?
-'https://indi-slides-app.herokuapp.com/' : 'http://localhost:4002';
+'https://indi-slides-app.herokuapp.com' : 'http://localhost:4002';
 
 export const getStudents = () => {
     return fetch(`${BASIC_API}/students`)
