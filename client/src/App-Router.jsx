a940b8e903d012fdbd62f784f7b9692c9c1ce7aa
Jacob Indalo Inggidou <jeck99@gmail.com>
2023-01-03 16:01:22 +0200

    Update App-Router.jsx

--- client/src/App-Router.jsx
+++ client/src/App-Router.jsx
@@ -1,14 +1,19 @@
-import { useContext } from "react";
+import { useContext, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/parts/Sidebar/Sidebar";
 import Header from "./components/parts/Header/Header";
 import Footer from "./components/parts/Footer/Footer";
-import PrivateRoute from "./components/parts/Private-Route/Private-Route";
-import Slides from "./components/pages/slides/slides";
-import Home from "./components/pages/Home/Home";
-import Login from "./components/pages/Login/Login";
+// import PrivateRoute from "./components/parts/Private-Route/Private-Route";
+// import Slides from "./components/pages/slides/slides";
+// import Home from "./components/pages/Home/Home";
+// import Login from "./components/pages/Login/Login";
 import { ThemeContext } from "./context/style-context";
 
+const Home = lazy(()=> import("./components/pages/Home/Home"));
+const Login = lazy(()=> import("./components/pages/Login/Login"));
+const PrivateRoute = lazy(()=> import("./components/pages/PrivateRoute/PrivateRoute"));
+const Slides = lazy(()=> import("./components/pages/slides/slides"));
+                  
 const AppRouter = ({ user }) => {
   const { darkMode, setDarkMode } = useContext(ThemeContext);
 
